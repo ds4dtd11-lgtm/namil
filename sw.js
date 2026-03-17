127398a344880e1955978d0c94e5398ccf88aa25
ds4dtd11-lgtm <ds4dtd11@gmail.com>
2026-03-18 07:04:08 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+// Service Worker - 항공방제 관리시스템
+const CACHE_NAME = 'hanbang-v1';
+const urlsToCache = [
+  '/namil/',
+  '/namil/index.html'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
